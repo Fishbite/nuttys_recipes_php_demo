b29d27e84e6039ed274f78badf62fbdc910c7b2e
Fishbite <43952420+Fishbite@users.noreply.github.com>
2025-03-26 14:54:21 +0000

    nav-main: updated links to nuttyskitchen.co.uk

--- views/partials/nav-main.view.php
+++ views/partials/nav-main.view.php
@@ -3,8 +3,8 @@
 
 <nav class="nav-main">
     <ul>
-        <li><a href="/">Home</a></li>
+        <li><a href="https://nuttyskitchen.co.uk">Home</a></li>
         <li><a href="/recipepage3.html">Recipes</a></li>
-        <li><a href="/contact-form.html">Contact Us</a></li>
+        <li><a href="https://nuttyskitchen.co.uk/contact-form.html">Contact Us</a></li>
     </ul>
 </nav>
